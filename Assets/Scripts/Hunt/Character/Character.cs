99664ffadc39f8c2593607e3722fc8863eb613da
newL <seydametov.e.i.2.16@gmail.com>
2020-04-18 20:21:59 +0300

    base game: v0.3.0 added platform prefabs

---
+++ Assets/Scripts/Hunt/Character/Character.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    //РЕФАКТОРИТЬ
+
+    [SerializeField] private float speed = 3.0f;
+    [SerializeField] private float jumpForce = 12.5f;
+
+    private bool isGrounded = false;
+
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
+
+
+    new private Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    private void Update()
+    {
+        if (isGrounded) State = CharState.Idle;
+        if (Input.GetButton("Horizontal")) Move();
+        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
+    }
+
+    private void Move()
+    {
+        State = CharState.Walking;
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+
+        sprite.flipX = direction.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+
+        isGrounded = colliders.Length > 1;
+    }
+}
+
+public enum CharState
+{
+    Idle,
+    Walking
+}
