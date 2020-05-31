f37772cb54c7a3aebaa1c9454dc87d40501ae275
newL <seydametov.e.i.2.16@gmail.com>
2020-05-31 19:38:45 +0300

    base game: v0.3.1a

---
+++ Assets/Scripts/GameTasksScripts/ClientSide/ResPrefabPointer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ResPrefabPointer : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    [SerializeField] private ResourcePrefabInfo prefabInfo;
+    [SerializeField] private Text code;
+    void Start()
+    {
+        prefabInfo = FindObjectOfType<ResourcePrefabInfo>();
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        gameObject.GetComponent<Image>().color = new Color32(0, 0, 0, 255);
+        prefabInfo.OnResourceFocus(code);
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        prefabInfo.Toggle(code);
+    }
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        gameObject.GetComponent<Image>().color = new Color32(0, 0, 0, 171);
+    }
+}
---
+++ Assets/Scripts/GameTasksScripts/ClientSide/ResourcePrefabInfo.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourcePrefabInfo : MonoBehaviour
+{
+    [SerializeField] private GameObject resourceInfoPanel;
+    [SerializeField] private Text m_ResName, m_ResCount;
+    [SerializeField] private Image m_ResIcon;
+    [SerializeField] private RectTransform m_Arrow;
+    /*Ресуры игрока*/
+    [SerializeField] private GetResourcePlayer getResource;
+    private bool m_IsSubmited;
+    private string chosenRes;
+    //При наведении на ресурс в панели
+    public void OnResourceFocus(Text code)
+    {
+        if (!m_IsSubmited)
+        {
+            m_Arrow.localRotation = Quaternion.Euler(0, 180, 0);
+            SetResourceInfo(code);
+        }    
+    }
+
+    //При выборе ресурса
+    public void Toggle(Text code)
+    {
+        if (!m_IsSubmited)
+        {
+            m_Arrow.localRotation = Quaternion.Euler(0, 0, 0);
+            m_IsSubmited = true;
+            chosenRes = code.text;
+        }
+        else
+        {
+            if (chosenRes.Equals(code.text))
+            {
+                m_Arrow.localRotation = Quaternion.Euler(0, 180, 0);
+                m_IsSubmited = false;
+            }
+            else
+            {
+                chosenRes = code.text;
+                SetResourceInfo(code);
+            }
+        }
+    }
+
+    //При НЕ наведении на ресурс в панели
+    public void ResourceUnFocus()
+    {
+        if(!m_IsSubmited) resourceInfoPanel.SetActive(false);
+    }
+
+
+    private void SetResourceInfo(Text code)
+    {
+        resourceInfoPanel.SetActive(true);
+        foreach (var model in getResource.playerResources)
+        {
+            if (code.text.Equals(model.code))
+            {
+                m_ResName.text = model.name.ToUpper();
+                m_ResCount.text = model.count.ToString();
+                m_ResIcon.sprite = Resources.Load<Sprite>("resources_icon/" + model.code);
+                break;
+            }
+        }
+    }
+}
